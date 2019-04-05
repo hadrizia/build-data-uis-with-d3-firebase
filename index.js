b519efceb42c9f699e80429b10cff25967d654ae
hadrizia <hadrizia@gmail.com>
2019-04-05 09:45:23 -0300

    Creating shapes using d3

---
+++ index.js
@@ -0,0 +1,34 @@
+const canvas = d3.select('.canvas');
+
+const svg = canvas.append('svg')
+  .attr('height', 600)
+  .attr('width', 600);
+
+//apend shapes to svg
+svg.append('rect')
+  .attr('width', 200)
+  .attr('height', 100)
+  .attr('fill', 'blue')
+  .attr('x', 20)
+  .attr('y', 20);
+
+svg.append('circle')
+  .attr('r', 50)
+  .attr('cx', 300)
+  .attr('cy', 70)
+  .attr('fill', 'pink')
+  ;
+
+svg.append('line')
+  .attr('x1', 370)
+  .attr('x2', 400)
+  .attr('y1', 20)
+  .attr('y2', 120)
+  .attr('stroke', 'red');
+
+svg.append('text')
+  .attr('x', 20)
+  .attr('y', 200)
+  .attr('fill', 'grey')
+  .text('hello')
+  .style('font-family', 'arial');
