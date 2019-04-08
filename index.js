65919484385841eb4a83a91b78bf109b399f43f3
Hadrizia Santos <hadrizia.santos@ccc.ufcg.edu.br>
2019-04-07 21:29:21 -0300

    Showing planets

--- index.js
+++ index.js
@@ -1,26 +1,19 @@
-const canvas = d3.select('.canvas');
+const svg = d3.select('svg');
 
-const data = [
-  { width: 200, height: 100, fill: 'pink' },
-  { width: 100, height: 60, fill: 'purple' },
-  { width: 50, height: 30, fill: 'red' }
+d3.json('planets.json')
+  .then(data => {
+    const circs = svg.selectAll('circle')
+    .data(data);
 
-]
+    circs.attr('r', d => { return d.radius })
+    .attr('cy', 200)
+    .attr('cx', d => { return d.distance })
+    .attr('fill', d => { return d.fill });
 
-const svg = canvas.select('svg');
-
-// Adicionando os dados  aos retângulos
-const rects = svg.selectAll('rect')
-  .data(data);
-
-// Definindo atributos para o rect já existente no html
-rects.attr('width', (d, i, n) => { return d.width })
-  .attr('height', d => { return d.height })
-  .attr('fill', d => { return d.fill });
-
-// Adicionando novos rects e definindo seus atributos de acordo com os dados
-rects.enter()
-  .append('rect')
-  .attr('width', (d, i, n) => { return d.width })
-  .attr('height', d => { return d.height })
-  .attr('fill', d => { return d.fill });
+    circs.enter()
+    .append('circle')
+    .attr('r', d => { return d.radius })
+    .attr('cy', 200)
+    .attr('cx', d => { return d.distance })
+    .attr('fill', d => { return d.fill });
+  });
